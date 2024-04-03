ae2b7e35e97a78ab68cff573eaf210d60780adde
Kengston <danidc@mail.ru>
2024-04-03 21:01:10 +0700

    User status backend-logic

---
+++ src/EventListener/LogoutSubscriber.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Http\Event\LogoutEvent;
+
+class LogoutSubscriber implements EventSubscriberInterface
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private UrlGeneratorInterface $urlGenerator
+    ) {
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [LogoutEvent::class => 'onLogout'];
+    }
+
+    public function onLogout(LogoutEvent $event): void
+    {
+        $user = $event->getToken()->getUser();
+
+        if ($user instanceof User) {
+            $user->setStatus('offline');
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+        }
+
+        $response = new RedirectResponse(
+            $this->urlGenerator->generate('app_login'),
+            RedirectResponse::HTTP_SEE_OTHER
+        );
+        $event->setResponse($response);
+    }
+}
