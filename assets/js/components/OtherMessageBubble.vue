571ff9b96bf8b12e3056bb0e6e6e5ce71ef2a56d
Kengston <danidc@mail.ru>
2024-05-02 19:15:26 +0700

    vue dropdown menu logic added

--- assets/js/components/OtherMessageBubble.vue
+++ assets/js/components/OtherMessageBubble.vue
@@ -20,23 +20,59 @@
           <template v-else>Delivered</template>
         </span>
       </div>
-
+      <button
+          :id="'dropdownMenuIconButton_' + message.id"
+          :data-dropdown-toggle="'dropdownDots_' + message.id"
+          data-dropdown-placement="bottom-start"
+          class="inline-flex self-center items-center p-2 text-sm font-medium text-center text-gray-900 bg-gray-200 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50"
+          type="button">
+        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 4 15">
+          <path d="M3.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 6.041a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 5.959a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"/>
+        </svg>
+      </button>
+      <div
+          :id="'dropdownDots_' + message.id"
+          class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-40 dark:bg-gray-700 dark:divide-gray-600">
+        <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownMenuIconButton">
+          <li>
+            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Reply</a>
+          </li>
+          <li>
+            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Forward</a>
+          </li>
+          <li>
+            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Copy</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { FwbDropdown, FwbListGroup, FwbListGroupItem } from 'flowbite-vue'
+  import {Dropdown} from "flowbite";
 
-export default {
-  components: {
-    FwbDropdown,
-    FwbListGroup,
-    FwbListGroupItem
-  },
-  props: {
-    message: Object,
-    selectedUser: Object
+  export default {
+    props: {
+      message: Object,
+      selectedUser: Object
+    },
+    data() {
+      return {
+        dropdowns: {}, // Storing all instances in `dropdowns` object.
+      };
+    },
+    created() {
+      // Initialize your dropdown instance when the component has been created.
+      this.dropdowns[this.message.id] = new Dropdown(
+          document.getElementById('dropdownDots_' + this.message.id),
+          document.getElementById('dropdownMenuIconButton_' + this.message.id)
+      );
+    },
+    methods: {
+      toggleDropdown(messageId) {
+        this.dropdowns[messageId].toggle();
+      },
+    },
   }
-}
 </script>
