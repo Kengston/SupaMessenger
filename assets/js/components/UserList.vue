3566b5ac5e5c69b586a839f1c89b7d67c58a0277
Kengston <danidc@mail.ru>
2024-05-15 21:45:13 +0700

    Search input functionality added

--- assets/js/components/UserList.vue
+++ assets/js/components/UserList.vue
@@ -1,20 +1,22 @@
 <template>
   <!-- Search Form Starts Here -->
-  <form class="max-w-md mx-auto">
-    <!-- Search Input -->
-    <div class="relative">
-      <label for="default-search" class="sr-only">Search</label>
-      <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-        <svg class="w-4 h-4 text-gray-500" aria-hidden="true" fill="none" viewBox="0 0 20 20">
-          <!-- Search Icon -->
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+  <div>
+    <div class="mb-3 relative">
+      <span class="px-3 py-2 leading-tight bg-white rounded-l text-sm border border-r-0 border-gray-200 cursor-not-allowed">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-4 w-4 text-gray-400 mx-auto">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
         </svg>
+      </span>
+      <input v-model="searchInput" @input="searchUsers" type="search" placeholder="Search Users" class="pl-10 outline-none rounded rounded-l px-6 py-3 w-full text-sm focus:border-blue-500 border border-l-0 border-gray-200 required" />
+      <div v-if="searchResults.length > 0">
+        <ul class="bg-white w-full border border-gray-100 rounded shadow-lg z-50 absolute">
+          <li v-for="result in searchResults" @click="selectUser(result)" :key="result.id" class="p-3 cursor-pointer hover:bg-gray-50">
+            {{ result.username }}
+          </li>
+        </ul>
       </div>
-      <!-- Search Input Field -->
-      <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search Users" required />
-      <!-- Search Button -->
     </div>
-  </form>
+  </div>
   <ul class="mt-5 space-y-5">
     <!-- Iterate throught users -->
     <li
@@ -43,9 +45,32 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
+  data() {
+    return {
+      searchInput: "",
+      searchResults: []
+    };
+  },
   props: ['users', 'currentUser', 'unreadMessageStatusArray'],
   methods: {
+    searchUsers() {
+      if (!this.searchInput) {
+        this.searchResults = [];
+        return;
+      }
+
+      // client side filtering
+      this.searchResults = this.users.filter(user =>
+          user.username.toLowerCase().includes(this.searchInput.toLowerCase())
+      );
+    },
+    selectUser(user) {
+      // currently this method does nothing
+      // this.searchInput = user.username;
+      // this.searchResults = [];
+    },
     getUserAvatar(user)
     {
       return '/avatars/' + (user.avatarFileName || 'user-tie-solid.svg')
