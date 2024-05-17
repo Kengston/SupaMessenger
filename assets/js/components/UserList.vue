5f6615dfa995b6e4ea4260a7c6f36e3018c98ab1
Kengston <danidc@mail.ru>
2024-05-17 20:38:21 +0700

    Delete all messages UI button added

--- assets/js/components/UserList.vue
+++ assets/js/components/UserList.vue
@@ -2,7 +2,7 @@
 <template>
   <!-- Search Form Starts Here -->
   <div>
-    <div class="mb-3 relative flex items-center">
+    <div class="m-2 relative flex items-center">
     <span class="absolute left-3 top-1/2 transform -translate-y-1/2">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-4 w-4 text-gray-400">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
