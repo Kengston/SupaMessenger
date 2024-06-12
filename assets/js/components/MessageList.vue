a48749937d958ec1d8b63b077d492c0edd7f8f2e
Kengston <danidc@mail.ru>
2024-06-12 23:53:43 +0700

    OnForward flash message added

--- assets/js/components/MessageList.vue
+++ assets/js/components/MessageList.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="message-list" class="flex flex-col h-full">
+
+    <FlashMessage v-bind="flash"/>
+
     <div class="overflow-y-auto flex-grow p-5 space-y-5" ref="scrollableDiv">
       <!-- Loop over messages -->
       <template v-for="(message, index) in currentMessages" :key="index">
@@ -36,16 +39,21 @@
 import OtherMessageBubble from './OtherMessageBubble'
 import UserMessageBubble from './UserMessageBubble'
 import MessageForm from "./MessageForm.vue";
+import FlashMessage from "./FlashMessage.vue";
+import { EventBus } from "../EventBus";
 
 export default {
   data() {
     return {
       currentMessages: this.messages,
-      replyMessage: null
+      replyMessage: null,
+      flash: { message: null, type: null },
     }
   },
   props: ['selectedUser', 'currentUser', 'mercureUrl', 'messages'],
   mounted() {
+    EventBus.on('show-flash', this.showFlash);
+
     this.scrollToBottom();
     const eventSource = new EventSource(this.mercureUrl);
     eventSource.onmessage = event => {
@@ -68,6 +76,13 @@ export default {
     this.scrollToBottom();
   },
   methods: {
+    showFlash(message, success) {
+      this.flash.message = message;
+      this.flash.type = success ? 'alert-success' : 'alert-error';
+      setTimeout(() => {
+        this.flash.message = null;
+      }, 2000);  // Flash message disappears after 2 seconds
+    },
     setReply(message) {
       this.replyMessage = message;
     },
@@ -80,10 +95,14 @@ export default {
       });
     }
   },
+  beforeDestroy() {
+    EventBus.off('message-sent', this.showFlash);
+  },
   components: {
     OtherMessageBubble,
     UserMessageBubble,
-    MessageForm
+    MessageForm,
+    FlashMessage
   },
 };
 </script>
