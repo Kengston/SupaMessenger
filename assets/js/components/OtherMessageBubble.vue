5e1807dadbdcb4b6a1c51b9d0e1dca9b1a0c4e89
Kengston <danidc@mail.ru>
2024-08-07 22:59:41 +0400

    Copy button functionality added

--- assets/js/components/OtherMessageBubble.vue
+++ assets/js/components/OtherMessageBubble.vue
@@ -96,7 +96,14 @@
       },
       replyToMessage() {
         this.$emit('reply', this.message);
-      }
+      },
+      copyToClipboard(text) {
+        navigator.clipboard.writeText(text).then(() => {
+          console.log('Text copied to clipboard');
+        }).catch(err => {
+          console.error('Failed to copy text: ', err);
+        });
+      },
     },
   }
 </script>
